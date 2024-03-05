8801ff9f2cd64714717c8286d84f670aba074900
johnburitto <johnburitto@gmail.com>
2024-03-05 14:04:15 +0200

    Creating User base class

---
+++ VMControlPanel/Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entities
+{
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public long TelegramId { get; set; }
+        public string? UserName { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? Email { get; set; }
+        public string? NormalizedUserName => UserName?.ToUpper();
+        public string? NormalizedEmail => Email?.ToUpper();
+    }
+}
