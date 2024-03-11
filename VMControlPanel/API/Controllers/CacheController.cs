de488e2ba3179982c792eee3f3fc0f5e865ee939
johnburitto <johnburitto@gmail.com>
2024-03-11 19:29:04 +0200

    Add controller for cache service

---
+++ VMControlPanel/API/Controllers/CacheController.cs
@@ -0,0 +1,45 @@
+﻿using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly ICacheService _service;
+
+        public CacheController(ICacheService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{key}")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> GetValueAsync(string key) 
+        {
+            return Ok(await _service.GetValueAsync<string>(key));
+        }
+
+        [HttpPost("{key}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> GetValueAsync(string key, string value, float expTimeInHours)
+        {
+            await _service.SetValueAsync(key, value, expTimeInHours);
+
+            return Ok();
+        }
+
+        [HttpDelete("{key}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> RemoveDataAsync(string key)
+        {
+            await _service.RemoveDataAsync(key);
+
+            return Ok();
+        }
+    }
+}
---
+++ VMControlPanel/API/Program.cs
@@ -0,0 +1,37 @@
+using Infrastructure.Services.Impls;
+using Infrastructure.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Dependencies
+builder.Services.AddScoped<ICacheService, CacheService>();
+
+// Add Redis caching
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
