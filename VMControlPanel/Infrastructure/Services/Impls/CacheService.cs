9a3e87d96a92d1afa206ff1f73b9524db6a1325d
johnburitto <johnburitto@gmail.com>
2024-03-11 18:45:48 +0200

    Add Redis cache service

---
+++ VMControlPanel/Infrastructure/Services/Impls/CacheService.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Services.Impls
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IDistributedCache _redis;
+
+        public CacheService(IDistributedCache redis)
+        {
+            _redis = redis;
+        }
+
+        public async Task<T?> GetValueAsync<T>(string key)
+        {
+            var stringData = await _redis.GetStringAsync(key);
+
+            return !string.IsNullOrEmpty(stringData) ? JsonConvert.DeserializeObject<T>(stringData) : default; 
+        }
+
+        public async Task SetValueAsync<T>(string key, T value, float expTimeInHours)
+        {
+            var stringData = JsonConvert.SerializeObject(value);
+            var options = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromHours(expTimeInHours))
+                .SetAbsoluteExpiration(DateTime.Now.AddHours(expTimeInHours));
+
+            await _redis.SetStringAsync(key, stringData, options);
+        }
+
+        public async Task RemoveDataAsync(string key)
+        {
+            await _redis.RemoveAsync(key);
+        }
+    }
+}
---
+++ VMControlPanel/VMControlPanelTest/Tests/CacheServiceTest.cs
@@ -0,0 +1,124 @@
+﻿using Infrastructure.Services.Impls;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace VMControlPanelTest.Tests
+{
+    public class CacheServiceTest
+    {
+        private readonly IDistributedCache _cache;
+        private readonly CacheService _underTest;
+
+        public CacheServiceTest()
+        {
+            var options = Options.Create(new MemoryDistributedCacheOptions());
+            
+            _cache = new MemoryDistributedCache(options);
+            _underTest = new CacheService(_cache);
+        }
+
+        [Fact]
+        public async Task GetValueAsync_NormalFlow()
+        {
+            // Given
+            var key = "some_key";
+            var data = "some_data";
+
+            await _underTest.SetValueAsync(key, data, 1);
+
+            // When
+            var result = await _underTest.GetValueAsync<string>(key);
+
+            // Then
+            Assert.Equal(data, result);
+        }
+
+        [Fact]
+        public async Task GetValueAsync_BadKey()
+        {
+            // Given
+            var key = "some_key";
+            var badKey = "bad_key";
+            var data = "some_data";
+
+            await _underTest.SetValueAsync(key, data, 1);
+
+            // When
+            var result = await _underTest.GetValueAsync<string>(badKey);
+
+            // Then
+            Assert.NotEqual(data, result);
+        }
+
+        [Fact]
+        public async Task SetValueAsync_NormalFlow()
+        {
+            // Given
+            var key = "some_key";
+            var data = "some_data";
+
+            // When
+            await _underTest.SetValueAsync(key, data, 1);
+            var result = await _underTest.GetValueAsync<string>(key);
+
+            // Then
+            Assert.Equal(data, result);
+        }
+
+        [Fact]
+        public async Task SetValueAsync_BadKey()
+        {
+            // Given
+            var key = "some_key";
+            var badKey = "bad_key";
+            var data = "some_data";
+
+
+            // When
+            await _underTest.SetValueAsync(key, data, 1);
+
+            var result = await _underTest.GetValueAsync<string>(badKey);
+
+            // Then
+            Assert.NotEqual(data, result);
+        }
+
+        [Fact]
+        public async Task RemoveDataAsync_NormalFlow()
+        {
+            // Given
+            var key = "some_key";
+            var data = "some_data";
+
+            await _underTest.SetValueAsync(key, data, 1);
+            
+            // When
+            await _underTest.RemoveDataAsync(key);
+
+            var result = await _underTest.GetValueAsync<string>(key);
+
+            // Then
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task RemoveDataAsync_BadKey()
+        {
+            // Given
+            var key = "some_key";
+            var badKey = "bad_key";
+            var data = "some_data";
+
+            await _underTest.SetValueAsync(key, data, 1);
+
+            // When
+            await _underTest.RemoveDataAsync(badKey);
+
+            var result = await _underTest.GetValueAsync<string>(key);
+
+            // Then
+            Assert.Equal(data, result);
+        }
+    }
+}
