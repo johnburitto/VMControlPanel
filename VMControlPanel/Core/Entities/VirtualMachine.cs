e070681e404680e88f31ae608bda84bf13b99048
johnburitto <johnburitto@gmail.com>
2024-03-20 13:28:12 +0200

    Create VM base class

---
+++ VMControlPanel/Core/Entities/VirtualMachine.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Services.Impls;
+
+namespace Core.Entities
+{
+    public class VirtualMachine
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public long UserTelegramId { get; set; }
+        public string? Username { get; set; }
+        public string? PasswordEncrypted { get; set; }
+        public string? Password { get => CryptoService.Blowfish(PasswordEncrypted, false); set { } }
+        public string? Host { get; set;}
+        public int Port { get; set; }
+    }
+}
