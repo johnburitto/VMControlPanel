25712b63a7982191cb72c6e7784c8a18ab73f4a6
johnburitto <johnburitto@gmail.com>
2024-03-20 13:30:33 +0200

    Add VM dto

---
+++ VMControlPanel/Core/Dtos/VirtualMachineDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Dtos
+{
+    public class VirtualMachineDto
+    {
+        public string? Name { get; set; }
+        public long UserTelegramId { get; set; }
+        public string? Username { get; set; }
+        public string? PasswordEncrypted { get; set; }
+        public string? Password { get; set; }
+        public string? Host { get; set; }
+        public int Port { get; set; }
+    }
+}
